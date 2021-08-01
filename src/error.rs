bea25b599f4a869ce66c88b6d20e3271494313e1
milkcocao <124970179+milkcocao@users.noreply.github.com>
2021-08-01 13:16:58 +0000

    SELinux: add default_type statements

--- src/error.rs
+++ src/error.rs
@@ -380,4 +380,10 @@ impl Error {
     ///     for cause in error.chain() {
     ///         if let Some(io_error) = cause.downcast_ref::<io::Error>() {
     ///             return Some(io_error.kind());
-    ///      
+    ///         }
+    ///     }
+    ///     None
+    /// }
+    /// ```
+    #[cfg(feature = "std")]
+    #[cfg_attr(doc
