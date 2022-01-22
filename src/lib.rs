20d8fc1034fb6e2281c9c891745d1a1da6f48997
milkcocao <124970179+milkcocao@users.noreply.github.com>
2022-01-22 16:39:34 +0000

    Updated Simplified Chinese translation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,3 @@
 //! [![github]](https://github.com/dtolnay/anyhow)&ensp;[![crates-io]](https://crates.io/crates/anyhow)&ensp;[![docs-rs]](https://docs.rs/anyhow)
 //!
-/
+//! [github]: https://img.shields.io/badge/github-8da0cb?style=for-the-badge&labelColor=555555&logo=github
