4094af2c3a40190861e0549c7354160bbd605bfe
milkcocao <124970179+milkcocao@users.noreply.github.com>
2022-01-22 18:13:18 +0000

    mm: fix corner case in anon_vma endless growing prevention

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 //! [![github]](https://github.com/dtolnay/anyhow)&ensp;[![crates-io]](https://crates.io/crates/anyhow)&ensp;[![docs-rs]](https://docs.rs/anyhow)
 //!
 //! [github]: https://img.shields.io/badge/github-8da0cb?style=for-the-badge&labelColor=555555&logo=github
+//! [crates-io]: https://img.shields.io/badge/crates.io-fc8d62?style=for-the-badge&labelColor=
