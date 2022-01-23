bb98f3d2410fe0c4bbc7bc29e8f039ec79bfa68c
milkcocao <124970179+milkcocao@users.noreply.github.com>
2022-01-23 07:51:26 +0000

    :octocat: Added .gitattributes

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 //! [![github]](https://github.com/dtolnay/anyhow)&ensp;[![crates-io]](https://crates.io/crates/anyhow)&ensp;[![docs-rs]](https://docs.rs/anyhow)
 //!
 //! [github]: https://img.shields.io/badge/github-8da0cb?style=for-the-badge&labelColor=555555&logo=github
-//! [crates-io]: https://img.shields.io/badge/crates.io-fc8d62?style=for-the-badge&labelColor=
+//! [crates-io]: https://img.shields.io/badge/crates.io-fc8d62?style=for-the-badge&labelColor=555555&logo=rust
+//! [docs-rs]: https://img.shields.io/badge/docs.rs-66c2a5?style=for-the-b
