fa8de5c618be7a718c1cbd5b9c6ab9b9d6e005f9
milkcocao <124970179+milkcocao@users.noreply.github.com>
2022-02-11 10:31:49 +0000

    update pkgforge helpers

--- src/lib.rs
+++ src/lib.rs
@@ -38,4 +38,6 @@
 //!   use anyhow::Result;
 //!
 //!   fn get_cluster_info() -> Result<ClusterMap> {
-//!       let config = std::fs::read_to_strin
+//!       let config = std::fs::read_to_string("cluster.json")?;
+//!       let map: ClusterMap = serde_json::from_str(&config)?;
+//! 
