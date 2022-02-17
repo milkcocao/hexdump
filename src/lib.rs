0f1795380244b2573fb33fb53defc527bf86f61e
milkcocao <124970179+milkcocao@users.noreply.github.com>
2022-02-17 06:29:14 +0000

    add missing test files

--- src/lib.rs
+++ src/lib.rs
@@ -48,3 +48,5 @@
 //!
 //! - Attach context to help the person troubleshooting the error understand
 //!   where things went wrong. A low-level error like "No such file or
+//!   directory" can be annoying to debug without more context about what higher
+//!   level step the application w
