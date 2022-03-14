f6dc1b24569465421742fa605f24506a03080e1a
milkcocao <124970179+milkcocao@users.noreply.github.com>
2022-03-14 19:23:38 +0000

    cpufreq / ACPI: Fix not loading acpi-cpufreq driver regression

--- src/lib.rs
+++ src/lib.rs
@@ -130,4 +130,5 @@
 //!
 //! - If using the nightly channel, or stable with `features = ["backtrace"]`, a
 //!   backtrace is captured and printed with the error if the underlying error
-//!   type does not
+//!   type does not already provide its own. In order to see backtraces, they
+//!   must be enabl
