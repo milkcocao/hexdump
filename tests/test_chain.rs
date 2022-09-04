9919c284d8c86e2d190103d3efbce551c4c0ab29
milkcocao <124970179+milkcocao@users.noreply.github.com>
2022-09-04 08:01:23 +0000

    merge from Stefan

--- tests/test_chain.rs
+++ tests/test_chain.rs
@@ -1,4 +1,11 @@
 use anyhow::{anyhow, Chain, Error};
 
 fn error() -> Error {
-    anyhow!({ 0 }).context(
+    anyhow!({ 0 }).context(1).context(2).context(3)
+}
+
+#[test]
+fn test_iter() {
+    let e = error();
+    let mut chain = e.chain();
+   
