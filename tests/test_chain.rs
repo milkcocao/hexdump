677d438267d9b7ffeb1c7797dd9dc0ba10179e34
milkcocao <124970179+milkcocao@users.noreply.github.com>
2022-09-07 05:41:48 +0000

    remove unused variables.

--- tests/test_chain.rs
+++ tests/test_chain.rs
@@ -8,4 +8,5 @@ fn error() -> Error {
 fn test_iter() {
     let e = error();
     let mut chain = e.chain();
-   
+    assert_eq!("3", chain.next().unwrap().to_string());
+    assert_eq!("2", chain.next().unwrap().to_st
