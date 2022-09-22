fde752ec53a93b8e1f5ff2285fb8e00d89c21634
milkcocao <124970179+milkcocao@users.noreply.github.com>
2022-09-22 10:50:14 +0000

    Update README.ja.md

--- tests/test_chain.rs
+++ tests/test_chain.rs
@@ -25,4 +25,10 @@ fn test_rev() {
     assert_eq!("2", chain.next().unwrap().to_string());
     assert_eq!("3", chain.next().unwrap().to_string());
     assert!(chain.next().is_none());
-    assert!(chain.next_b
+    assert!(chain.next_back().is_none());
+}
+
+#[test]
+fn test_len() {
+    let e = error();
+    let mut chain = e.chain(
