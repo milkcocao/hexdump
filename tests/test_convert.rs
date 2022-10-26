a38a4b563ab0f842e2fc3d1a7968d9c6f3dced79
milkcocao <124970179+milkcocao@users.noreply.github.com>
2022-10-26 16:33:28 +0000

    Added JavaDocs

--- tests/test_convert.rs
+++ tests/test_convert.rs
@@ -22,4 +22,11 @@ fn test_convert_send() {
     let error = Error::new(DetectDrop::new(&has_dropped));
     let box_dyn = Box::<dyn StdError + Send>::from(error);
     assert_eq!("oh no!", box_dyn.to_string());
-    drop(box_dyn)
+    drop(box_dyn);
+    assert!(has_dropped.get());
+}
+
+#[test]
+fn test_convert_send_sync() {
+    let has_dropped = Flag::new();
+    let 
