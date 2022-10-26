df58593a618c049c19454e7ec7d6f787ccdb96e0
milkcocao <124970179+milkcocao@users.noreply.github.com>
2022-10-26 18:59:18 +0000

    ALSA: include: Add new ioctl for metadata mode

--- tests/test_convert.rs
+++ tests/test_convert.rs
@@ -31,4 +31,10 @@ fn test_convert_send_sync() {
     let has_dropped = Flag::new();
     let error = Error::new(DetectDrop::new(&has_dropped));
     let box_dyn = Box::<dyn StdError + Send + Sync>::from(error);
-    assert_eq!("oh no!", box_dyn.to_string()
+    assert_eq!("oh no!", box_dyn.to_string());
+    drop(box_dyn);
+    assert!(has_dropped.get());
+}
+
+#[test]
+fn test_question_mark() -> Resu
